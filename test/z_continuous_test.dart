e03fad753f7266d199896c5eb9ffeb31ab5bb265
Chart Chongcharoen <chartchuo@gmail.com>
2022-11-07 22:41:39 +0700

    add continuous with ack

--- test/z_continuous_test.dart
+++ test/z_continuous_test.dart
@@ -22,7 +22,35 @@ void main() {
   group('all', () {
     test('continuous', () async {
       var client = Client();
-      await client.connect(Uri.parse('ws://localhost:8080'));
+      await client.connect(Uri.parse('nats://localhost:4222'));
+      var sub = client.sub('iso');
+      var r = 0;
+
+      sub.stream.listen((msg) {
+        if (r % 1000 == 0) {
+          // print(msg.string);
+        }
+        r++;
+      });
+
+      var receivePort = ReceivePort();
+      var iso = await Isolate.spawn(run, receivePort.sendPort);
+      var out = await receivePort.first;
+      // print(out);
+      expect(out, equals('finish'));
+      iso.kill();
+      //wait for last message send round trip to server
+      await Future.delayed(Duration(seconds: 3));
+
+      await sub.close();
+      await client.close();
+
+      expect(r, equals(iteration));
+    });
+    test('continuous with ack', () async {
+      var client = Client();
+      await client.connect(Uri.parse('nats://localhost:4222'),
+          connectOption: ConnectOption(verbose: true));
       var sub = client.sub('iso');
       var r = 0;
 
