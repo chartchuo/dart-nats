d098c0c961cb62976125d9e55e8d38dd462f5731
Cory Hagerman <cory.hagerman@dematic.com>
2023-06-28 12:19:28 -0400

    Fixing some typos

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -23,13 +23,13 @@ class Header {
     return this;
   }
 
-  /// get valure from key
+  /// get value from key
   /// return null if notfound
   String? get(String key) {
     return headers![key];
   }
 
-  /// constructure from bytes
+  /// construct from bytes
   static Header fromBytes(Uint8List b) {
     var str = utf8.decode(b);
     Map<String, String> m = {};
@@ -47,7 +47,7 @@ class Header {
     return Header(headers: m, version: version);
   }
 
-  /// conver to bytes
+  /// convert to bytes
   Uint8List toBytes() {
     var str = '${this.version}\r\n';
 
@@ -86,21 +86,21 @@ class Message<T> {
     return jsonDecoder!(string);
   }
 
-  ///constructure
+  ///constructor
   Message(this.subject, this.sid, this.byte, this._client,
       {this.replyTo, this.jsonDecoder, this.header});
 
   ///payload in string
   String get string => utf8.decode(byte);
 
-  ///Repond to message
+  ///Respond to message
   bool respond(Uint8List data) {
     if (replyTo == null || replyTo == '') return false;
     _client.pub(replyTo, data);
     return true;
   }
 
-  ///Repond to string message
+  ///Respond to string message
   bool respondString(String str) {
     return respond(utf8.encode(str) as Uint8List);
   }
